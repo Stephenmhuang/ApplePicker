12adc8d35d1908380e181dd2af3c8914c0dbb3bc
Stephenmhuang <Stephenmhuang@live.com>
2017-06-16 22:08:45 -0500

    ApplePicker
    Very odd. I have written the rest of the code after the baskets destroyed part but I am still not sure exactly why the apples dont drop anymore and also why my baskets just dissapear

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -3,13 +3,58 @@ using System.Collections;
 
 public class AppleTree : MonoBehaviour {
 
+	// instantiating apples
+	public GameObject applePrefab;
+
+	//speed of apple tree in meters
+	public float speed =1f;
+
+	//distance where apple tree turns around
+	public float leftAndRightEdge = 10f;
+
+	//chance that the apple tree will change directions
+	public float chanceToChanceDirections = 0.1f;
+
+	// rate at which apple tree will be instantiated
+	public float secondsBetweenAppleDrops = 1f;
+
+
 	// Use this for initialization
 	void Start () {
+
+		InvokeRepeating ("DropApple", 2f, secondsBetweenAppleDrops);
 	
+	}
+
+	void DropApple() {
+		GameObject apple = Instantiate (applePrefab) as GameObject;
+		apple.transform.position = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		//change direction
+
+		if ( pos.x < -leftAndRightEdge ) {
+			speed = Mathf.Abs (speed); //move right
+		} else if ( pos.x > leftAndRightEdge ) {
+			speed = -Mathf.Abs (speed); //move left
+		} 
+
+
 	
+
 	}
+	void FixedUpdate() {
+
+		if (Random.value < chanceToChanceDirections) {
+			speed *= -1; //change direction
+		}
+	}
+
 }
--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -3,13 +3,47 @@ using System.Collections;
 
 public class Basket : MonoBehaviour {
 
+	public GUIText scoreGT;
+
 	// Use this for initialization
 	void Start () {
-	
+		GameObject scoreGO = GameObject.Find ("ScoreCounter");
+		scoreGT = scoreGO.GetComponent<GUIText> ();
+		scoreGT.text = "0";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	//get current screen position of mouse from input
+		Vector3 mousePos2D = Input.mousePosition;
+
+		//camera's z position sets the how far to push the mouse into 3d
+		mousePos2D.z = -Camera.main.transform.position.z;
+
+		//convert the point from 2d screen space into 3d game world space
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+
+		//move x pos of basket to x pos of mouse
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos;
 	}
+
+	void OnCollisionEnter ( Collision coll) {
+		GameObject collidedWith = coll.gameObject;
+		if (collidedWith.tag == "Apple") {
+			Destroy (collidedWith);
+		}
+
+		int score = int.Parse (scoreGT.text);
+		score += 100;
+		scoreGT.text = score.ToString ();
+
+
+		//Track high score
+		if (score > HighScore.score) {
+			HighScore.score = score;
+		}
+	}
+
 }
